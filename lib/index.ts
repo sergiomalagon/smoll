c38ef6af0ef33a48214da6b3d03c940996aad61e
sergiomalagon <sergiomalagonmartin@gmail.com>
2024-03-31 20:48:46 +0200

    added Smoll class

---
+++ lib/index.ts
@@ -0,0 +1,57 @@
+// early return if the http version is not compatible
+
+import http from 'node:http';
+import type { ServerOptions, IncomingMessage, ServerResponse } from 'node:http';
+
+interface SmollRoutes {
+  [path: string]: {
+    method: 'GET';
+    handle: (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => void;
+  };
+}
+
+interface SmollContructor extends ServerOptions {
+  routes?: SmollRoutes;
+  /**
+   * @todo Add docs
+   * @default ```localhost```
+   */
+  host?: string;
+  /**
+   * @todo Add docs
+   * @default ```0```
+   */
+  port?: number;
+}
+
+export default class Smoll {
+  routes: SmollRoutes = {};
+
+  constructor(opts?: SmollContructor) {
+    this.routes = opts?.routes || {};
+
+    http.createServer(this.callback().bind(this)).listen({
+      host: opts?.host,
+      port: opts?.port,
+    });
+  }
+
+  private callback() {
+    return function (this: Smoll, req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+      let handle;
+
+      if (req?.url) {
+        handle = this.routes[req.url];
+
+        if (handle && handle.method === req.method) {
+          handle.handle(req, res);
+          return;
+        }
+      }
+    };
+  }
+}
+
+export { Smoll };
+
+export type { SmollRoutes}
